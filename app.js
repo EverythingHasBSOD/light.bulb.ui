37b9a919e16ef70fb39ad24e2ca35a55df1c0d48
EverythingHasBSOD <qasimlimurad92@gmail.com>
2025-04-12 12:12:26 +0400

    12

---
+++ app.js
@@ -0,0 +1,18 @@
+// JSCODE START
+
+// varibles
+let img = document.getElementById('image');
+
+// func off
+function func_off() {
+ img.src = './img/bulb_off.gif'
+ console.log
+}
+
+
+//func on
+function func_on() {
+ img.src = './img/bulb_on.gif'
+ console.log
+}
+// JSCODE END
